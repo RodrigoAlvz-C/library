b03edf748e2946cdf125107254c08dc4bff4295e
gorz <c.rodrigoalvz@gmail.com>
2024-08-02 14:23:50 -0300

    displayBooks function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const content = document.getElementById('content')
+
 const library = []
 
 function Book(title, author, pages, isRead) {
@@ -8,7 +10,7 @@ function Book(title, author, pages, isRead) {
 }
 
 Book.prototype.toggleRead = function () {
-  this.isRead === true ? this.isRead = false : this.isRead = true 
+  this.isRead ? this.isRead = false : this.isRead = true 
 }
 
 const addBookToLibrary = (title, author, pages, isRead) => {
@@ -16,3 +18,46 @@ const addBookToLibrary = (title, author, pages, isRead) => {
   library.push(book)
 }
 
+const displayBooks = () => {
+  library.forEach((book, index) => {
+
+    const card = document.createElement('div')
+    card.dataset.index = index
+    card.classList.add('card')
+
+    const del = document.createElement('button')
+    del.textContent = 'x'
+    del.classList.add('delete-btn')
+
+    const title = document.createElement('div')
+    title.textContent = book.title
+    title.classList.add('card-title')
+
+    const author = document.createElement('div')
+    author.textContent = `By ${book.author}`
+    author.classList.add('card-author')
+    
+    const pages = document.createElement('div')
+    pages.textContent = `Pages: ${book.pages}`
+    pages.classList.add('card-pages')
+
+    const status = document.createElement('div')
+    status.textContent = `Status: ${book.isRead ? 'read' : 'not read yet'}`
+    status.classList.add('card-status')
+
+    card.appendChild(del)
+    card.appendChild(title)
+    card.appendChild(author)
+    card.appendChild(pages)
+    card.appendChild(status)
+    content.appendChild(card)
+  })
+}
+
+
+
+
+
+addBookToLibrary('Oathbringer','Brandon Sanderson', 1248, true)
+addBookToLibrary('The Hero of Ages','Brandon Sanderson', 741 , true)
+addBookToLibrary('Lord of Chaos','Robert Jordan', 720, true)
